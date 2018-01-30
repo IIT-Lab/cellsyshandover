cad23a2d164febd40f6014aa9d89fe28d57d9b7c
Ritesh Ghanta <ritesh.0927@outlook.com>
2018-01-31 01:14:10 +0530

    removed shapely and descartes dependencies

--- cellsys/draw.py
+++ cellsys/draw.py
@@ -14,10 +14,8 @@ import numpy as np
 import pylab as pl
 import math
 import random
+import matplotlib.patches as patches
 
-from shapely.geometry import Point
-from shapely.geometry.polygon import Polygon
-from descartes import PolygonPatch
 from mpl_toolkits.mplot3d import Axes3D
 
 from .geom import *
@@ -57,35 +55,36 @@ class draw(object):
         y = cj + (ci * np.sin(np.pi / 6))
         center = np.asarray([2 * self.redge * x, 2 * self.redge * y])
         vertices = [(center + z) for z in self.basis]
-        hexg = Polygon(vertices)
         eclr = [chr(keepzero(ord(vl) - 1)) for vl in fclr]
         eclr[0] = '#'
         axis = fig.gca()
-        axis.add_patch(PolygonPatch(hexg, fc=fclr, ec=''.join(eclr)))
+        axis.add_patch(patches.RegularPolygon((center[0], center[1]), 6, self.radius, orientation=(np.pi/2), fc=fclr, ec=''.join(eclr)))
 
     def drawHexSectored(self, ci, cj, fig, fclr):
         x = ci * np.cos(np.pi / 6)
         y = cj + (ci * np.sin(np.pi / 6))
         center = np.asarray([2 * self.redge * x, 2 * self.redge * y])
         vertices = [(center + z) for z in self.basis]
-        hexg = Polygon(vertices)
         sverts = [
             [vertices[0], vertices[1], center, vertices[5]],
             [vertices[1], vertices[2], vertices[3], center],
             [vertices[3], vertices[4], vertices[5], center]
         ]
-        sectors = [Polygon(verts) for verts in sverts]
         axis = fig.gca()
-        xx, yy = hexg.exterior.xy
-        pl.plot(xx, yy, color='#DB3236')
+        axis.add_patch(patches.Polygon(np.asarray(vertices), fill=False, ec="#DB3236", zorder=50))
         for i in range(0, 3):
-            axis.add_patch(PolygonPatch(sectors[i], fc=fclr[i], ec=fclr[i]))
+            axis.add_patch(patches.Polygon(np.asarray(sverts[i]), fc=fclr[i], ec=fclr[i]))
 
     def drawTiers(self, ntiers, off, fig, clr):
         #######
         x = off[0] * cc(np.pi / 6)
         y = off[1] + (off[0] * ss(np.pi / 6))
-        pl.scatter(2 * self.redge * x, 2 * self.redge * y, s=2, facecolors='none', color='#00FF00', zorder=100)
+        if(off == (0, 0)):
+            pl.scatter(2 * self.redge * x, 2 * self.redge * y, s=10,
+                        facecolors='none', color='#FFFFFF', zorder=100)
+        else:
+            pl.scatter(2 * self.redge * x, 2 * self.redge * y, s=10,
+                        facecolors='none', color='#00FF00', zorder=100)
         #######
         p = -1 * ntiers
         q = 0
@@ -108,6 +107,11 @@ class draw(object):
             p += 1
 
     def drawTiersSectored(self, ntiers, off, fig, clr):
+        ax = fig.gca()
+        figsz = math.ceil((2 * (ntiers + 1)) * self.redge)
+        ax.set_xlim(-figsz, figsz)
+        ax.set_ylim(-figsz, figsz)
+        ax.set_aspect('equal')
         geome = geom(1.0)
         # darker colors
         dclr = clr[1]
@@ -137,9 +141,50 @@ class draw(object):
             r -= 1
             p += 1
 
+    def drawTiersSectoredHet(self, ntiers, incells, off, fig, clr):
+        ax = fig.gca()
+        figsz = math.ceil((2 * (ntiers + 1)) * self.redge)
+        ax.set_xlim(-figsz, figsz)
+        ax.set_ylim(-figsz, figsz)
+        ax.set_aspect('equal')
+        geome = geom(1.0)
+        # darker colors
+        dclr = clr[1]
+        lclr = clr[0]
+        #######
+        x = off[0] * cc(np.pi / 6)
+        y = off[1] + (off[0] * ss(np.pi / 6))
+        pl.scatter(2 * self.redge * x, 2 * self.redge * y, s=2, facecolors='none', color='#00FF00', zorder=100)
+        #######
+        p = -1 * ntiers
+        q = 0
+        r = ntiers
+        while(q < ntiers):
+            for i in range(p, q + 1):
+                if((i, r) in incells):
+                    if(geome.checkReuseSectored((i, r))):
+                        self.drawHexSectored(i + off[0], r + off[1], fig, dclr)
+                    else:
+                        self.drawHexSectored(i + off[0], r + off[1], fig, lclr)
+                else:
+                    self.drawHex(i + off[0], r + off[1], fig, "#EEEEEE")
+            r -= 1
+            q += 1
+        while(p <= 0):
+            for i in range(p, q + 1):
+                if((i, r) in incells):
+                    if(geome.checkReuseSectored((i, r))):
+                        self.drawHexSectored(i + off[0], r + off[1], fig, dclr)
+                    else:
+                        self.drawHexSectored(i + off[0], r + off[1], fig, lclr)
+                else:
+                    self.drawHex(i + off[0], r + off[1], fig, "#EEEEEE")
+            r -= 1
+            p += 1
+
     def drawLayout(self, ntiers, freuse, cells, fig):
         ax = fig.gca()
-        figsz = math.ceil((3 * ntiers) * self.redge)
+        figsz = math.ceil((2 * (ntiers + 1) + 1) * self.redge)
         ax.set_xlim(-figsz, figsz)
         ax.set_ylim(-figsz, figsz)
         ax.set_aspect('equal')
@@ -150,6 +195,9 @@ class draw(object):
             #clr = "#%06x" % random.randint(0, 0xFFFFFF)
             clr = "#EEEEEE"
             self.drawTiers(ntiersToPlot, cell, fig, clr)
+        pl.scatter(0, 0, s=10,
+                    facecolors='none', color='#00FF00', zorder=0,
+                    label="Interfering Base Stations")
 
     def drawPointsInHexagon(self, pointsList, hexCenter, hexRadius, fig):
         # Plot of random user points in the hexagon
@@ -166,8 +214,10 @@ class draw(object):
 
         xsc, ysc = zip(*pointsList)
         #self.updateRadius(hexRadius)
-        self.drawHex(0, 0, fig, "#EEEEEE")
-        pl.scatter(xsc, ysc, s=0.75, color='#000000', zorder=200)
+        #self.drawHex(0, 0, fig, "#EEEEEE")
+        ax = fig.gca()
+        ax.scatter(xsc, ysc, s=1.0, color='#DB3236', zorder=200, label='Users')
+        ax.legend(scatterpoints=5)
 
     def updateRadius(self, radius):
         self.radius = radius
